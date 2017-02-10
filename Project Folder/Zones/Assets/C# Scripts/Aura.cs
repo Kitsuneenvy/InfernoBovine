71e3f0090404cd059ab5c80ec68cebedc6d133ac
KitsuneEnvy <lachlangodthelp@hotmail.com>
2017-02-11 05:25:28 +1100

    Little bit of work on auras and started work on networking.

---
+++ Project Folder/Zones/Assets/C# Scripts/Aura.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aura : MonoBehaviour {
+
+	public int auraRange = 5;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<CircleCollider2D>().radius = auraRange;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void AuraEffects(){
+		
+	}
+}
---
+++ Project Folder/Zones/Assets/C# Scripts/NetworkVersions/SelectionBoxNetwork.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SelectionBoxNetwork : NetworkBehaviour {
+
+
+	public bool enabledBox = false;
+	float lifespan = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer){
+			return;
+		}
+		if(enabledBox){
+			if(this.GetComponent<BoxCollider2D>().size.x<0.1f){
+				this.GetComponent<BoxCollider2D>().size = new Vector2(0.1f,this.GetComponent<BoxCollider2D>().size.y);
+			}
+			if(this.GetComponent<BoxCollider2D>().size.y<0.1f){
+				this.GetComponent<BoxCollider2D>().size = new Vector2(this.GetComponent<BoxCollider2D>().size.x,0.1f);
+			}
+			lifespan-=Time.deltaTime;
+			if(lifespan<=0){
+				GameObject.Destroy(this.gameObject,0.01f);
+			}
+		}
+	}
+
+//	void OnTriggerStay2D(Collider2D collider){
+//		if(enabledBox){
+//			if(!Camera.main.gameObject.GetComponent<GameGUI>().selectedObjects.Contains(collider.gameObject)){
+//				Camera.main.gameObject.GetComponent<GameGUI>().selectedObjects.Add(collider.gameObject);
+//				Camera.main.gameObject.GetComponent<GameGUI>().newSelection = true;
+//			}
+//		}
+//	}
+	void OnTriggerEnter2D(Collider2D collider){
+		if(enabledBox){
+			if(!Camera.main.gameObject.GetComponent<GameGUINetwork>().selectedObjects.Contains(collider.gameObject)){
+				Camera.main.gameObject.GetComponent<GameGUINetwork>().selectedObjects.Add(collider.gameObject);
+				Camera.main.gameObject.GetComponent<GameGUINetwork>().newSelection = true;
+			}
+		}
+	}
+
+}
